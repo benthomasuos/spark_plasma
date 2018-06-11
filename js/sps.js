011b56f442aaa9b56357e3112b3d7019ea4ca378
Ben Thomas <b.thomas@sheffield.ac.uk>
2018-06-11 17:16:27 +0100

    Added some images

--- js/sps.js
+++ js/sps.js
@@ -13,3 +13,23 @@ $('.maintenance_sop').on('click', function(d, i){
 	$('')
 
 })
+
+
+$(document).ready(function(){
+	$('img').each(function(){
+		console.log("Setting up")
+		setupImg($(this))
+
+	})
+
+	
+
+})
+
+
+function setupImg(img){
+	var url = img.attr('src')
+	img.wrap("<a href='" + url + "'></a>")
+
+
+}
